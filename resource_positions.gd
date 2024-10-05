c66bd1ae9cf9a08e762f93f64c20ecd0785d4f9c
Polanas <ivanhilya@gmail.com>
2024-10-05 18:42:25 +0300

    first

--- resource_positions.gd
+++ resource_positions.gd
@@ -1,10 +1,7 @@
 extends Node2D
 
-class_name Leaves
-
-@export
-var test : int = 5
-
+@export 
+var positions: Array[Vector2] = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
--- spawn.gd
+++ spawn.gd
@@ -5,18 +5,33 @@ var object_scene = preload("res://leaves.tscn")
 var button_scene = preload("res://buttons.tscn")  # Загрузка сцены кнопок
 
 # Number of objects to create
-var number_of_objects = 5
+var number_of_objects = 10
+var min_spawn_distance = 150
+var max_iterations = 100
 
 func _ready():
 	var viewport_size = get_window().size
-	var spawn_instance = get_node("MainNode")
+	var random_positions: Array[Vector2] = []
 	
 	for i in range(number_of_objects):
+		var iterations = 0
+		while true:
+			var random_position = Vector2(randf_range(0, viewport_size.x), randf_range(0, viewport_size.y))
+			var can_insert = true
+			for pos in random_positions:
+				if (random_position - pos).length() < min_spawn_distance: 
+					can_insert = false
+			if can_insert:
+				random_positions.push_front(random_position)
+				break
+			iterations += 1
+			if iterations > max_iterations:
+				random_positions.push_front(random_position)
+				print("Maximum amount of iterations was achieved while searching for avalible position for leafs")
+				break
+	for pos in random_positions:
 		var instance : Leaves = object_scene.instantiate()
-		
-		var random_position = Vector2(randf_range(0, viewport_size.x / 1.3), randf_range(0, viewport_size.y / 1.3))
-		instance.position = random_position
-		
+		instance.position = pos
 		add_child(instance)
 		
 		# Создаем и добавляем кнопки
@@ -26,19 +41,15 @@ func _ready():
 		buttons_instance.position = instance.position - Vector2(0, 60)
 		
 		add_child(buttons_instance)
+#
 		### Подписываемся на сигналы кнопок
-		buttons_instance.get_node("ButtonPlus").connect("pressed", _on_ButtonPlus_pressed.bind(instance, buttons_instance))
-		buttons_instance.get_node("ButtonMinus").connect("pressed", _on_ButtonMinus_pressed.bind(instance, buttons_instance))
-	
-func _on_ButtonPlus_pressed(instance, buttons_instance):
-	_update_workers_label(instance, buttons_instance, instance.workers + 1)
+		buttons_instance.get_node("ButtonPlus").connect("pressed", _on_ButtonPlus_pressed.bind(instance, ))
+		buttons_instance.get_node("ButtonMinus").connect("pressed", _on_ButtonMinus_pressed.bind(instance))
+		
+func _on_ButtonPlus_pressed(instance):
+	instance.test += 1
+	print(instance.test, " incremented by 1")
 
-func _on_ButtonMinus_pressed(instance, buttons_instance):
-	_update_workers_label(instance, buttons_instance, instance.workers - 1)
-
-func _update_workers_label(instance, buttons_instance, workers_value):
-	if workers_value < 0:
-		return
-	instance.workers = workers_value
-	var workers_label = buttons_instance.get_node("WorkersLabel")
-	workers_label.text = "[center]" + str(workers_value) + "[/center]"
+func _on_ButtonMinus_pressed(instance):
+	instance.test -= 1
+	print(instance.test, " decremented by 1")
